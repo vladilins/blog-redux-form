a3c648aa91aabe2046deb5481e7eaf42d18c74de
vladilin <ilin96inbox@gmail.com>
2020-06-16 20:54:49 +0900

    validate form

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -15,7 +15,11 @@ export class PostNew extends Component {
     return (
       <form>
         <Field name="title" label="Title" component={this.renderField}></Field>
-        <Field name="tags" label="Tag" component={this.renderField}></Field>
+        <Field
+          name="categories"
+          label="Categories"
+          component={this.renderField}
+        ></Field>
         <Field
           name="content"
           label="Post Content"
@@ -26,8 +30,28 @@ export class PostNew extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "Enter a title!";
+  }
+
+  if (!values.categories) {
+    errors.categories = "Enter some categories";
+  }
+
+  if (!values.content) {
+    errors.content = "Enter some content";
+  }
+
+  // if errors empty will submit the form
+  return errors;
+}
+
 PostNew = reduxForm({
   form: "PostsNewForm",
+  validate,
 })(PostNew);
 
 export default PostNew;
