4067700518679b9ad78f35997f1b0809e6e58eb9
vladilin <ilin96inbox@gmail.com>
2020-06-18 08:31:00 +0900

    add link to post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { fetchPost } from "../actions/index";
 
 class PostsShow extends Component {
   componentDidMount() {
-    const { id } = this.props.match.params;
-    this.props.fetchPost(id);
+    if (!this.props.post) {
+      const { id } = this.props.match.params;
+      this.props.fetchPost(id);
+    }
   }
 
   render() {
@@ -17,6 +20,7 @@ class PostsShow extends Component {
 
     return (
       <div>
+        <Link to="/">Back to Index</Link>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
