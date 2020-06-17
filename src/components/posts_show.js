d92c49e60102d368afcbf8c7d33a6b122a06fb1f
vladilin <ilin96inbox@gmail.com>
2020-06-17 11:54:54 +0900

    fetch post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { fetchPost } from "../actions";
 
 export class PostsShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
   render() {
     return <div>Posts Show</div>;
   }
 }
 
-const mapStateToProps = (state) => ({});
+const mapStateToProps = ({ posts }, ownProps) => ({
+  post: posts[ownProps.match.params.id],
+});
 
-const mapDispatchToProps = {};
+const mapDispatchToProps = {
+  fetchPost,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostsShow);
